afb54c449ae7629b46323c116e157f59f592e331
adger.lai <adger.lai@DEV-ADGER.shijicloud.local>
2018-07-13 17:46:09 +0800

    添加笔记：1.富文本编辑器 2.多图上传

---
+++ Teacher/6_MVC-V3+FuWenBenEdit/App/Admin/Views/index/index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AdminLTE 2 | Simple Tables</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="./Assets/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="./Assets/admin/bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="./Assets/admin/bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="./Assets/admin/dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="./Assets/admin/dist/css/skins/_all-skins.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Google Font -->
+<!--    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">-->
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+    <?php //include "./App/Admin/Views/common/header_aside.php";?>
+    <?php include display("common/header_aside"); ?>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                商品管理
+            </h1>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="box">
+                        <div class="box-header">
+                            <h3 class="box-title">商品列表</h3>
+
+                            <div class="box-tools">
+                                <div class="row">
+                                    <div class="col-xs-5">
+                                        <div class="input-group input-group-sm" style="width: 150px;">
+                                            <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+                                            <div class="input-group-btn">
+                                                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-4 col-xs-offset-2">
+                                        <a href="./admin.php?act=index&view=addform" type="button" class="btn btn-success">添加</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body table-responsive no-padding">
+                            <table class="table table-hover">
+                                <tr>
+                                    <th>商品编号</th>
+                                    <th>商品名称</th>
+                                    <th>商品图片</th>
+                                    <th>商品价格</th>
+                                    <th>商品数量</th>
+                                    <th>商品分类</th>
+<!--                                    <th>商品详情</th>-->
+                                    <th>操作</th>
+                                </tr>
+                                <?php foreach ($glist as $val){ ?>
+                                    <tr>
+                                        <td><?php echo $val["id"] ?></td>
+                                        <td><?php echo $val["sname"] ?></td>
+                                        <td><img src="<?php echo $val["pic"] ?>" height="50px" alt="">
+                                        </td>
+                                        <td class="redfont"><?php echo $val["price"] ?></td>
+                                        <td><?php echo $val["storenum"] ?></td>
+                                        <!--            <td>--><?php //echo $val["cname"] ?><!--</td>-->
+                                        <td><?php echo $gcm->getCnameByCid($val["cid"]) ?></td>
+<!--                                        <td>--><?php //echo $val["detail"] ?><!--</td>-->
+                                        <td>
+                                            <a href="./admin.php?act=index&view=showupd&id=<?php echo $val['id'] ?>">编辑</a> |
+                                            <a href="./admin.php?act=index&view=dodel&id=<?php echo $val['id'] ?>">删除</a>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            </table>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+            </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+            <b>Version</b> 2.4.0
+        </div>
+        <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+        reserved.
+    </footer>
+
+    <!-- /.control-sidebar -->
+    <!-- Add the sidebar's background. This div must be placed
+         immediately after the control sidebar -->
+    <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="./Assets/admin/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="./Assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Slimscroll -->
+<script src="./Assets/admin/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="./Assets/admin/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="./Assets/admin/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="./Assets/admin/dist/js/demo.js"></script>
+</body>
+</html>
