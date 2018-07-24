6ecf3dc6929fdf05d6ab8f2892c67d1cc514c2f7
adger.lai <adger.lai@DEV-ADGER.shijicloud.local>
2018-07-24 18:42:51 +0800

    1.图片上传功能完善

--- propsMallNo2Backstage/app/admin/ctrl/goodsEditAddCtrl.php
+++ propsMallNo2Backstage/app/admin/ctrl/goodsEditAddCtrl.php
@@ -24,7 +24,8 @@ if ($act === 'edit') {
     $imgSrcList = s($goodsId);
 }
 
-function s($goodsId){
+function s($goodsId)
+{
     $goodsImgSrc = "../../../uploads/" . $goodsId;
     if (is_dir($goodsImgSrc) !== true) {
         return null;
@@ -33,6 +34,7 @@ function s($goodsId){
         $imgSrcList = array();
         foreach ($file as $key => $img) {
             if ($img != "." && $img != "..") {
+                $imgSrcList[$key - 2]["id"] = $goodsId . "-" . ($key - 2);
                 $imgSrcList[$key - 2]["name"] = $img;
                 $imgSrcList[$key - 2]["src"] = $goodsImgSrc . "/" . $img;
             }
@@ -61,7 +63,7 @@ if ($act === 'add') {
     ));
 }
 
-/*
+/* *
  * 上传图片
  * */
 if ($act === "image_upload") {
@@ -91,22 +93,31 @@ if ($act === "image_upload") {
 
     /*5.返回数据接口*/
     if ($issuc) {
-//        $dir = dirname("$mainDir/$goods_id/$filename");
-//        $file = scandir($dir);
-//        $imgSrc = array();
-//        foreach ($file as $key => $img) {
-//            if ($img != "." && $img != "..") {
-//                $imgSrc[$key - 2]["name"] = $img;
-//                $imgSrc[$key - 2]["src"] = "./$mainDir/$goods_id/" . $img;
-//            }
-//        }
-        $imgSrc = s($goods_id);
-        echo json_encode(["code" => 1, "msg" => "文件上传成功", "filepath" => "./$mainDir/$goods_id/$filename", "goods_id" => "$goods_id", "file" => $imgSrc]);
+        $imgSrcList = s($goods_id);
+        echo json_encode(["code" => 1, "msg" => "文件上传成功", "filepath" => "./$mainDir/$goods_id/$filename", "goods_id" => "$goods_id", "file" => $imgSrcList]);
     } else {
         echo json_encode(["code" => 0, "msg" => "文件上传失败", "filepath" => "./$mainDir/$goods_id/$filename"]);
     }
 }
+/* *
+ * 删除图片
+ * */
+if ($act === "image_delete") {
+    $deleteData = $_POST['deleteData'];
 
+    $goods_id = $deleteData['goodsId'];
+    $deleteImgSrc = $deleteData['deleteImgSrc'];
+    if (file_exists($deleteImgSrc)) {
+        if (!unlink($deleteImgSrc)) {
+            echo json_encode(["code" => 0, "msg" => "未知原因，删除失败"]);
+        } else {
+            $imgSrcList = s($goods_id);
+            echo json_encode(["code" => 1, "msg" => "文件删除成功", "goods_id" => "$goods_id", "file" => $imgSrcList]);
+        }
+    } else {
+        echo json_encode(["code" => 0, "msg" => "文件不存在"]);
+    }
+}
 
 //echo "<script>console.log('goodsEditAddCtrl')</script>";
 
--- propsMallNo2Backstage/app/admin/view/common/imagesUpload.php
+++ propsMallNo2Backstage/app/admin/view/common/imagesUpload.php
@@ -8,80 +8,58 @@
         height: 30px;
         transition: all .5s;
     }
+
     .uploader-list-container .placeholder {
         border: none;
         color: #999;
     }
 </style>
 <div class="formControls col-10">
-
-
     <div class="uploader-list-container">
         <div class="queueList">
-
-                <div id="dndArea" class="placeholder">
-                    <div id="filePicker-2" class="webuploader-container">
-                        <div class="webuploader-pick">点击选择图片</div>
-                        <div id="rt_rt_1cijcqbmh2b06oc1fgftm11o0m1"
-                             style="position: absolute; top: 20px; left: 0px; width: 168px; height: 44px; overflow: hidden; bottom: auto; right: auto;">
-                            <input multiple="multiple" type="file" id="uppic" style="display: none"
-                                   onchange="doupbyajax()"><label for="uppic"
-                                                                  style="opacity: 0; width: 100%; height: 100%; display: block; cursor: pointer; background: rgb(255, 255, 255);"></label>
-                        </div>
+            <div id="dndArea" class="placeholder">
+                <div id="filePicker-2" class="webuploader-container">
+                    <div class="webuploader-pick">点击选择图片</div>
+                    <div id="rt_rt_1cijcqbmh2b06oc1fgftm11o0m1"
+                         style="position: absolute; top: 20px; left: 0px; width: 168px; height: 44px; overflow: hidden; bottom: auto; right: auto;">
+                        <input multiple="multiple" type="file" id="uppic" style="display: none"
+                               onchange="doupbyajax()"><label for="uppic"
+                                                              style="opacity: 0; width: 100%; height: 100%; display: block; cursor: pointer; background: rgb(255, 255, 255);"></label>
                     </div>
-                    <p>请上传商品图片，支持多图上传！</p>
                 </div>
-
+                <p>请上传商品图片，支持多图上传！</p>
+            </div>
             <ul class="filelist">
                 <?PHP if (isset($imgSrcList) && $imgSrcList != "") {
-                    foreach ($imgSrcList as $src) { ?>
-                        <li id="goods-img-"><p class="title"><?PHP echo $src['name'] ?></p>
+                    foreach ($imgSrcList as $key => $src) { ?>
+                        <li id="goods-img-<?PHP echo $src['id'] ?>"><p class="title"><?PHP echo $src['name'] ?></p>
                             <p class="imgWrap"><img src="<?PHP echo $src['src'] ?>"></p>
-                            <div class="file-panel"><span class="cancel">删除</span></div>
+                            <div class="file-panel"><span onclick="deleteImg(this)" class="cancel">删除</span></div>
                         </li>
                         <?PHP
                     }
                 } ?>
-
             </ul>
         </div>
-
-
-            <div class="statusBar" style="" id="statusBar">
-                <div class="progress" style="display: none;">
-                    <span class="text">0%</span>
-                    <span class="percentage" style="width: 0%;"></span>
-                </div>
-
-
-                <div class="info">选中1张图片，共124.74K。</div>
-                <div class="btns">
-
-                    <!--                <div id="filePicker2" class="webuploader-container">
-                                        <div class="webuploader-pick">继续添加</div>
-                                        <div id="rt_rt_1cij1s054skmdm91rgar9k1ue37"
-                                             style="position: absolute; top: 0px; left: 10px; width: 94px; height: 42px; overflow: hidden; bottom: auto; right: auto;">
-                                            <input type="file" name="file" class="webuploader-element-invisible" multiple="multiple"><label
-                                                    style="opacity: 0; width: 100%; height: 100%; display: block; cursor: pointer; background: rgb(255, 255, 255);"></label>
-                                        </div>
-                                    </div>-->
-
-                    <div class="uploadBtn state-ready">
-                        <label for="uppic">继续添加</label>
-                        <input multiple="multiple" type="file" id="uppic" style="display: none" onchange="doupbyajax()">
-                    </div>
+        <div class="statusBar" style="" id="statusBar">
+            <div class="progress" style="display: none;">
+                <span class="text">0%</span>
+                <span class="percentage" style="width: 0%;"></span>
+            </div>
+            <div class="info">选中1张图片，共124.74K。</div>
+            <div class="btns">
+                <div class="uploadBtn state-ready">
+                    <label for="uppic">继续添加</label>
+                    <input multiple="multiple" type="file" id="uppic" style="display: none" onchange="doupbyajax()">
                 </div>
             </div>
+        </div>
 
 
     </div>
 </div>
 
-
-<img src="<?php echo isset($picvalue) ? $picvalue : "" ?>" id="showpic" height="100px">
 <script>
-
-
 
 
     function doupbyajax() {
@@ -94,6 +72,11 @@
         }
     }
 
+    srcTip = false;
+
+    /*
+    * 上传图片
+    * */
     function postImg(imgFrom) {
         $.ajax({
             url: "../ctrl/goodsEditAddCtrl.php?goods_act=image_upload&goods_id=<?PHP echo $val['id'] ?>",//上传文件接收服务
@@ -104,43 +87,92 @@
             contentType: false,    //不可缺
             processData: false,    //不可缺
             success: function (data) {
-
                 if (data.code !== 1) {
 
                 } else {
-
-
-                    console.log(data);
-                    ssss = "";
+                    // console.log(data);
+                    var imgSrc = "";
                     for (var src in data.file) {
-                        ssss += '<li id="goods-img-' + src + '"><p class="title">' + data.file[src].name + '</p>\n' +
-                            '                    <p class="imgWrap"><img src="../../../' + data.file[src].src + '"></p>\n' +
-                            '                    <div class="file-panel"><span class="cancel">删除</span></div>\n' +
+                        imgSrc += '<li id=" goods-img-' + data.file[src].id + ' "><p class="title">' + data.file[src].name + '</p>\n' +
+                            '                    <p class="imgWrap"><img src="' + data.file[src].src + '"></p>\n' +
+                            '                    <div class="file-panel"><span onclick="deleteImg(this)" class="cancel">删除</span></div>\n' +
                             '                </li>';
 
                     }
                     $('.filelist').html('');
-                    $('.filelist').append(ssss);
-                    console.log(ssss);
+                    $('.filelist').append(imgSrc);
+                    imgTip(srcTip = true);
                 }
-                // $("#showpic").attr("src",data.filepath);//设置图片的src属性值
-                // $("#picurl").val(data.filepath);//返回路径放入表单元素
             }
         });
     }
 
-$(function () {
-    function f() {
 
-        var s = "<?PHP echo isset($imgSrcList); ?>"
-        if(s){
-            $('#dndArea').css('display','none');
-            $('.statusBar').css('display','block');
-        }else {
-            $('#dndArea').css('display','block');
-            $('.statusBar').css('display','none');
+    /*
+    * 删除图片操作
+    * */
+    function deleteImg(tis) {
+        var deleteImgSrc = $(tis).parent().siblings('.imgWrap').children('img').attr('src');
+        var deleteData = {
+            "deleteImgSrc": deleteImgSrc,
+            "goodsId":<?PHP echo $val['id'] ?>,
+        };
+        // console.log(deleteData);
+        $.ajax({
+            url: "../ctrl/goodsEditAddCtrl.php?goods_act=image_delete",//删除文件
+            type: 'POST',
+            data: {
+                "deleteData": deleteData
+            },
+            dataType: "json",
+            success: function (data) {
+                // console.log(data);
+                if (data.code !== 1) {
+                    console.log(data);
+                } else {
+                    console.log(data);
+                    var imgSrc = "";
+                    for (var src in data.file) {
+                        imgSrc += '<li id=" goods-img-' + data.file[src].id + ' "><p class="title">' + data.file[src].name + '</p>\n' +
+                            '                    <p class="imgWrap"><img src="' + data.file[src].src + '"></p>\n' +
+                            '                    <div class="file-panel"><span onclick="deleteImg(this)" class="cancel">删除</span></div>\n' +
+                            '                </li>';
+
+                    }
+                    $('.filelist').html('');
+                    $('.filelist').append(imgSrc);
+                    imgTip(srcTip = false);
+                }
+            }
+        });
+    }
+
+
+    /*
+    * 图片上传提示
+    * */
+    function imgTip(Tip) {
+
+        var imgList = "<?PHP echo isset($imgSrcList); ?>";//图片列表是否存在
+        var imgSrcListLength = "<?PHP echo count($imgSrcList); ?>";//图片列表是否存在数据 不存在长度为0
+        var imgListLength = $('.filelist').children('li').length;//列表父元素下 子元素的个数 无图为0
+        imgSrcListLength = imgListLength;
+        if(imgListLength === 0){imgList = 0;console.log("无图1");}
+        if ( imgList || Tip ) {
+            console.log("有图", imgListLength, imgSrcListLength, imgList, srcTip);
+            console.log();
+            $('#dndArea').css('display', 'none');
+            $('.statusBar').css('display', 'block');
+        } else {
+            console.log("无图3", imgListLength, imgSrcListLength, imgList, srcTip);
+            $('#dndArea').css('display', 'block');
+            $('.statusBar').css('display', 'none');
         }
     }
-    f();
-});
+
+    imgTip();
+
+    $(function () {
+
+    });
 </script>
